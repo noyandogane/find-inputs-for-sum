b309bb13affb79c29614c2bc2fe8a59de8306dd2
fromagery <66711531+fromagery@users.noreply.github.com>
2022-10-03 21:53:08 +0300

    changed the output to indexes of the numbers

--- girdi-bul.py
+++ girdi-bul.py
@@ -1,6 +1,3 @@
-from tkinter import N
-
-
 def find_numbers(numbers, result):
     numbers = input("Enter the numbers with spaces in-between: ").split()
     result = int(input("Enter the result: "))
@@ -9,21 +6,18 @@ def find_numbers(numbers, result):
     for i in range(len(numbers)):
         for j in range(i + 1, len(numbers)):
             if numbers[i] + numbers[j] == result:
-                
-                return [i, j]
+                return [numbers[i], numbers[j]]
 
 
 while True:
     numbers = []
     result = 0
     numbers = find_numbers(numbers, result)
-    
 
     if numbers is None:
         print("No numbers found!")
     else:
-        print("You can get the result by adding the numbers at these indexes together: ", numbers)
-
+        print("You can get the result by adding these numbers together: ", numbers)
 
     if input("Do you want to restart? (y/n): ") == "n":
         print("Exiting...")
